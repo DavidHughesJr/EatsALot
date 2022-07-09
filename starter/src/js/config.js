6ce04d1ef1d3bc8b734998e01650348709d974df
DavidHughesJr <85458666+DavidHughesJr@users.noreply.github.com>
2022-07-09 13:21:36 -0500

    update

--- starter/src/js/config.js
+++ starter/src/js/config.js
@@ -1,4 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
-export const RESULTS_PER_PAGE = 10;
-export const PAGE = 1
+export const RES_PER_PAGE = 10;
+export const KEY = 'd97395bd-820a-4d23-b6b6-0cc38b2a5690';
+export const MODAL_CLOSE_SEC = 2.5;
